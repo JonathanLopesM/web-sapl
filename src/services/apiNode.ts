a701b109c843937abf2bdb7c07e4e7adf829b94b
Jonathan Lopes Machado <jonathan.lopes1004@gmail.com>
2023-09-08 01:44:17 +0000

    updated on route

--- src/services/apiNode.ts
+++ src/services/apiNode.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const api = axios.create({
   // @ts-ignore
-  baseURL: "http://localhost:3333" // import.meta.env.VITE_URL_API_NODE,
+  baseURL: import.meta.env.VITE_URL_API_NODE,
 })
 // "http://localhost:3333"
 
